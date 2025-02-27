0d65c67da46048132158d3f579e40dd83c5cce2e
ricardodev04 <rapineda@innovacion.gob.sv>
2025-02-27 10:21:51 -0600

    2.9: La Agenda Telefónica Paso 4

--- agenda/src/App.jsx
+++ agenda/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const [ persons, setPersons ] = useState([
     { 
       name: 'Arto Hellas',
       number: '040-123456'
+    },
+    { 
+      name: 'Ricardo',
+      number: '123456789'
+    },
+    { 
+      name: 'Jaime',
+      number: '789456123'
+    },
+    { 
+      name: 'Fidel',
+      number: '456123789'
     }
   ]) 
-  const [newName, setNewName] = useState('');
+  const [ newName, setNewName] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
+  const [ search, setSearch ] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
@@ -34,9 +47,19 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase()) // Devuelve true si encuentra coincidencia
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      Search: <input value={ search } onChange={ handleSearchChange } />
+      <h2>Add a new contact</h2>
       <form onSubmit={addName}>
         <div>
           Name: <input value={ newName } onChange={ handleNameChange }/>
@@ -49,7 +72,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map(person => <p key={person.name}> { person.name } { person.number }</p>)}
+      {
+        filteredPersons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+        ))
+      }
     </div>
   )
 }
